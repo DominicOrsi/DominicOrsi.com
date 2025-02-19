bfc9850bad3efb76190a0c56fa6d8d083a951737
DominicOrsi <dominic.orsi@gmail.com>
2025-02-18 22:27:20 -0800

    Updating experience and footer

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -9,7 +9,21 @@ export default function Experience() {
             <h2 className="font-gloria text-2xl ">Experience</h2>
             <div className="flex flex-col gap-8">
                 <ExperienceComp
-                    jobTitle="Assistant System Administrator"
+                    jobTitle="Assocaite System Administrator"
+                    company="Expeditors"
+                    startToEndDate="October 2024 - Present"
+                    description="I managed 3,500+ Unix servers, ensuring reliability and efficient resource allocation across a global infrastructure. I performed system upgrades, including adding storage, RAM, CPU, and swap space, to enhance machine performance and support growing workloads. I proactively diagnosed and resolved system issues, minimizing downtime and ensuring continuous operations. Additionally, I developed automation solutions using Python, Bash, and Ansible playbooks, streamlining repetitive administrative tasks and freeing up valuable team time."
+                />
+                <hr className="h-px w-9/12 bg-slate-200 self-center" />
+                <ExperienceComp
+                    jobTitle="IT Customer Support"
+                    company="Washington State Univeristy"
+                    startToEndDate="May 2024 - September 2024"
+                    description="I  provided technical assistance to 1,300+ students and faculty, ensuring the smooth operation of hardware, software, and network resources across university labs and offices. I managed and prioritized IT support tickets using Jira, escalating critical issues to maintain efficiency and streamline workflows. Additionally, I delivered phone and in-person support, guiding users through troubleshooting steps and documenting resolutions for future reference. I also assisted with system updates and software installations, ensuring compliance with university policies and security best practices."
+                />
+                <hr className="h-px w-9/12 bg-slate-200 self-center" />
+                <ExperienceComp
+                    jobTitle="Junior System Administrator"
                     company="Gonzaga University"
                     startToEndDate="December 2021 - May 2024"
                     description="I have extensive experience supporting a diverse community of 500+ students and faculty in their day-to-day operations and lab activities. Additionally, I have successfully implemented a custom Linux image to cater to the needs of over 100 students across two classrooms, ensuring efficient software and hardware deployment. My expertise also extends to configuring and automating software and operating systems, as well as proactively maintaining and upgrading them to guarantee optimal performance and security."
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
     return (
         <footer className="flex flex-col justify-center text-center gap-8 max-w-lg border-t-2 border-slate-700 pt-8">
             <div className="text-slate-400">
-                <p> © 2024 Dominic Orsi. All rights reserved.</p>
+                <p> © 2025 Dominic Orsi. All rights reserved.</p>
             </div>
-            <p>Made with 🫶 by Dominic Orsi 🐻</p>
+            <p>Made with 🫶 by Dominic Orsi</p>
         </footer>
     )
 }
